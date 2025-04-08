f98a0c1b9185abb3b7fb84804058814b22c0d374
KayoBS <kayobs@hotmail.com>
2025-04-08 20:58:15 -0300

    Atualização da q1

--- q1/Calculator.java
+++ q1/Calculator.java
@@ -4,6 +4,7 @@ public class Calculator
 {
     private int valueA;
     private int valueB;
+    private int result;
     private char op;
 
     //get value a
@@ -13,6 +14,9 @@ public class Calculator
     public int getvb() {
         return this.valueB;
     }
+    public int getr() {
+        return this.result;
+    }
     public void setva( int valueA ) {
         this.valueA = valueA;
     }
@@ -20,9 +24,48 @@ public class Calculator
         this.valueB = valueB;
     }
 
-    private testValue( int valueX );
-    public boolean testOperator();
-    public int calculate();
-    public void show();
-    public void captureInformations();
+    private boolean testValue( int valueX ) {
+        if( (valueX <= 100) && (valueX >= -100) ) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+    public boolean testOperator() {
+        switch (this.op) {
+            case '+' -> return true;
+            case '-' -> return true;
+            case '/' -> return true;
+            case '*' -> return true;
+            default -> return false;
+        }
+    }
+    public int calculate() {
+        switch (this.op) {
+            case '+' -> this.result = (this.valueA+this.valueB);
+            case '-' -> this.result = (this.valueA-this.valueB);
+            case '/' -> this.result = (this.valueA/this.valueB);
+            case '*' -> this.result = (this.valueA*this.valueB);
+            default  -> return -1;
+        }
+        return this.result;
+    }
+    public void show() {
+        System.print.out("\033c");
+        System.print.out("\n" + String.valueOf(getva()) + " ");
+        System.print.out( op + " " + String.valueOf(getvb()) );
+        System.print.out( " = " < String.valueOf(getr()) );
+    }
+    public void captureInformations() {
+        Scanner scan = new Scanner(System.in);
+
+        String capture = scan.nextLine();
+        int sz = capture.length();
+        String va, vb;
+        char op;
+
+        for( int i = 0 ; (capture[i]!="+" && capture[i]!="-" && capture[i]!="*" && capture[i]!="/") ; ++i ) {
+
+        }
+    }
 }
